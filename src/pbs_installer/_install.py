3b6abea359ef35516521129703f664e2463a608c
Frost Ming <me@frostming.com>
2024-03-22 10:08:48 +0800

    feat: switch to httpx as the HTTP client
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pbs_installer/_install.py
+++ src/pbs_installer/_install.py
@@ -12,7 +12,7 @@ from ._utils import PythonVersion, get_arch_platform, unpack_tar
 logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
-    import requests
+    import httpx
     from _typeshed import StrPath
 
 THIS_ARCH, THIS_PLATFORM = get_arch_platform()
@@ -33,10 +33,18 @@ def get_download_link(
 ) -> tuple[PythonVersion, str]:
     """Get the download URL matching the given requested version.
 
-    :param request: The version of Python to install, e.g. 3.8,3.10.4
-    :param arch: The architecture to install, e.g. x86_64, arm64
-    :param platform: The platform to install, e.g. linux, macos
-    :return: A tuple of the PythonVersion and the download URL
+    Parameters:
+        request: The version of Python to install, e.g. 3.8,3.10.4
+        arch: The architecture to install, e.g. x86_64, arm64
+        platform: The platform to install, e.g. linux, macos
+
+    Returns:
+        A tuple of the PythonVersion and the download URL
+
+    Examples:
+        >>> get_download_link("3.10", "x86_64", "linux")
+        (PythonVersion(kind='cpython', major=3, minor=10, micro=13),
+        'https://github.com/indygreg/python-build-standalone/releases/download/20240224/cpython-3.10.13%2B20240224-x86_64-unknown-linux-gnu-pgo%2Blto-full.tar.zst')
     """
     from ._versions import PYTHON_VERSIONS
 
@@ -59,39 +67,44 @@ def get_download_link(
     raise ValueError(f"Could not find a CPython {request!r} matching this system")
 
 
-def _read_sha256(url: str, sess: requests.Session) -> str | None:
-    resp = sess.get(url + ".sha256", headers=_get_headers())
-    if not resp.ok:
+def _read_sha256(url: str, client: httpx.Client) -> str | None:
+    resp = client.get(url + ".sha256", headers=_get_headers())
+    if not resp.is_success:
         logger.warning("No checksum found for %s, this would be insecure", url)
         return None
     return resp.text.strip()
 
 
-def download(url: str, destination: StrPath, session: requests.Session | None = None) -> str:
+def download(url: str, destination: StrPath, client: httpx.Client | None = None) -> str:
     """Download the given url to the destination.
 
-    :param url: The url to download
-    :param destination: The file path to download to
-    :param session: A requests session to use for downloading, or None to create a new one
-    :return: The original filename of the downloaded file
+    Note: Extras required
+        `pbs-installer[download]` must be installed to use this function.
+
+    Parameters:
+        url: The url to download
+        destination: The file path to download to
+        client: A http.Client to use for downloading, or None to create a new one
+
+    Returns:
+        The original filename of the downloaded file
     """
     logger.debug("Downloading url %s to %s", url, destination)
-    filename = unquote(url.rsplit("/")[-1])
     try:
-        import requests
+        import httpx
     except ModuleNotFoundError:
-        raise RuntimeError("You must install requests to use this function") from None
+        raise RuntimeError("You must install httpx to use this function") from None
 
-    if session is None:
-        session = requests.Session()
+    if client is None:
+        client = httpx.Client(trust_env=True, follow_redirects=True)
 
+    filename = unquote(url.rsplit("/")[-1])
     hasher = hashlib.sha256()
-    checksum = _read_sha256(url, session)
+    checksum = _read_sha256(url, client)
 
-    with open(destination, "wb") as f:
-        response = session.get(url, stream=True, headers=_get_headers())
-        response.raise_for_status()
-        for chunk in response.iter_content(chunk_size=8192):
+    with open(destination, "wb") as f, client.stream("GET", url, headers=_get_headers()) as resp:
+        resp.raise_for_status()
+        for chunk in resp.iter_bytes(chunk_size=8192):
             if checksum:
                 hasher.update(chunk)
             f.write(chunk)
@@ -106,9 +119,13 @@ def install_file(
 ) -> None:
     """Unpack the downloaded file to the destination.
 
-    :param filename: The file to unpack
-    :param destination: The directory to unpack to
-    :param original_filename: The original filename of the file, if it was renamed
+    Note: Extras required
+        `pbs-installer[install]` must be installed to use this function.
+
+    Parameters:
+        filename: The file to unpack
+        destination: The directory to unpack to
+        original_filename: The original filename of the file, if it was renamed
     """
 
     import tarfile
@@ -141,18 +158,28 @@ def install(
     request: str,
     destination: StrPath,
     version_dir: bool = False,
-    session: requests.Session | None = None,
+    client: httpx.Client | None = None,
     arch: str | None = None,
     platform: str | None = None,
 ) -> None:
     """Download and install the requested python version.
 
-    :param request: The version of Python to install, e.g. 3.8,3.10.4
-    :param destination: The directory to install to
-    :param version_dir: Whether to install to a subdirectory named with the python version
-    :param session: A requests session to use for downloading
-    :param arch: The architecture to install, e.g. x86_64, arm64
-    :param platform: The platform to install, e.g. linux, macos
+    Note: Extras required
+        `pbs-installer[all]` must be installed to use this function.
+
+    Parameters:
+        request: The version of Python to install, e.g. 3.8,3.10.4
+        destination: The directory to install to
+        version_dir: Whether to install to a subdirectory named with the python version
+        client: A httpx.Client to use for downloading
+        arch: The architecture to install, e.g. x86_64, arm64
+        platform: The platform to install, e.g. linux, macos
+
+    Examples:
+        >>> install("3.10", "./python")
+        Installing cpython@3.10.4 to ./python
+        >>> install("3.10", "./python", version_dir=True)
+        Installing cpython@3.10.4 to ./python/cpython@3.10.4
     """
     ver, url = get_download_link(request, arch=arch, platform=platform)
     if version_dir:
@@ -161,5 +188,5 @@ def install(
     os.makedirs(destination, exist_ok=True)
     with tempfile.NamedTemporaryFile() as tf:
         tf.close()
-        original_filename = download(url, tf.name, session)
+        original_filename = download(url, tf.name, client)
         install_file(tf.name, destination, original_filename)
